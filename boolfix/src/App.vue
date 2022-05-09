e8bc4a2350cda4686ec202274400d46d17c1aac7
FedericoCapitani <federico.capitani@outlook.it>
2022-05-09 13:39:03 +0200

    add v-for to print films in output

--- boolfix/src/App.vue
+++ boolfix/src/App.vue
@@ -4,6 +4,14 @@
       <input type="text" v-model="searchText">
       <button v-on:click="sendCall">Invia</button>
     </header>
+    <main>
+      <div class="card" v-for="film in data" :key="film.id">
+        <div>Titolo: {{film.title}} </div>
+        <div>Titolo originale: {{film.original_title}} </div>
+        <div>Lingua: {{film.original_language}} </div>
+        <div>Rating: {{film.vote_average}} </div>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -24,7 +32,7 @@ export default {
     callApi(){
       axios.get("https://api.themoviedb.org/3/search/movie?api_key=629aec4e8c473eea3b4989c3e1430c08&language=en-US&page=1&include_adult=false&query=" + this.searchText).then((response) => {
         console.log(response);
-        this.data = response.data;
+        this.data = response.data.results;
         console.log(this.data);
       })
       .catch((error) => {
