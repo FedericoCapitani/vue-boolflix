3b42278746bd5099a87c37f96d9a70acc50ff2a5
FedericoCapitani <federico.capitani@outlook.it>
2022-05-09 14:43:54 +0200

    add api ucall for tv series

--- boolfix/src/App.vue
+++ boolfix/src/App.vue
@@ -5,11 +5,24 @@
       <button v-on:click="sendCall">Invia</button>
     </header>
     <main>
-      <div class="card" v-for="film in data" :key="film.id">
-        <div>Titolo: {{film.title}} </div>
-        <div>Titolo originale: {{film.original_title}} </div>
-        <div>Lingua: {{film.original_language}} </div>
-        <div>Rating: {{film.vote_average}} </div>
+      <div class="output">
+
+        <div class="card" v-for="film in films" :key="film.id">
+          <div>Titolo: {{film.title}} </div>
+          <div>Titolo originale: {{film.original_title}} </div>
+          <div>Lingua: {{film.original_language}} </div>
+          <div>Rating: {{film.vote_average}} </div>
+        </div>
+
+        <h1>SEPARATORE</h1>
+
+        <div class="card" v-for="show in TV_series" :key="show.id">
+          <div>Titolo: {{show.name}} </div>
+          <div>Titolo originale: {{show.original_name}} </div>
+          <div>Lingua: {{show.original_language}} </div>
+          <div>Rating: {{show.vote_average}} </div>
+        </div>
+
       </div>
     </main>
   </div>
@@ -25,22 +38,31 @@ export default {
   data() {
     return{
       searchText: '',
-      data: null
+      films: null,
+      TV_series: null
     }
   },
   methods: {
     callApi(){
       axios.get("https://api.themoviedb.org/3/search/movie?api_key=629aec4e8c473eea3b4989c3e1430c08&language=en-US&page=1&include_adult=false&query=" + this.searchText).then((response) => {
         console.log(response);
-        this.data = response.data.results;
-        console.log(this.data);
+        this.films = response.data.results;
+        console.log(this.films);
       })
       .catch((error) => {
         console.error();
         error;
         this.error = `Sorry There is a problem! ${error}`;        
       });
-
+      axios.get("https://api.themoviedb.org/3/search/tv?api_key=629aec4e8c473eea3b4989c3e1430c08&language=en-US&page=1&include_adult=false&query=" + this.searchText).then((response) => {
+        console.log(response);
+        this.TV_series = response.data.results;
+        console.log(this.TV_series);
+      }).catch((error) => {
+        console.error();
+        error;
+        this.error = `Sorry There is a problem! ${error}`;
+      })
     },
   sendCall(){
     // la chiamata avviene solo se l'utente scrive qualcosa nella searchbar
